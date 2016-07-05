74132f8353aa44f0c09a2245c65e23a38fd05d92
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2016-07-04 20:09:42 -0400

    Added html and R code for testing.

---
+++ predicting_activities.R
@@ -0,0 +1,7 @@
+## ------------------------------------------------------------------------
+library(ggplot2)
+library(caret)
+
+## ----code=readLines(knitr::purl('./predicting_activities.Rmd', documentation = 1)), eval = FALSE----
+## NA
+
