9f8da468a31b26dea503eb51d0abde924a2086e6
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2016-07-08 18:42:50 -0400

    Making sure everything is correctly formatted before actually running the model.

--- README.md
+++ README.md
@@ -14,8 +14,17 @@ thing that people regularly do is quantify how much of a particular
 activity they do, but they rarely quantify how well they do it. In
 this project, your goal will be to use data from accelerometers on the
 belt, forearm, arm, and dumbell of 6 participants. They were asked to
-perform barbell lifts correctly and incorrectly in 5 different
-ways. More information is available from the website here:
+perform barbell lifts correctly and incorrectly in 4 different ways:
+.
+- class A: exactly according to the specification, 
+- class B: throwing the elbows to the front,
+- class C: lifting the dumbbell only halfway,
+- class D: lowering the dumbbell only halfway,
+- class E: throwing the hips to the front.
+
+
+
+More information is available from the website here:
 [http://groupware.les.inf.puc-rio.br/har](http://groupware.les.inf.puc-rio.br/har)
 (see the section on the Weight Lifting Exercise Dataset).
 
--- predicting_activities.R
+++ predicting_activities.R
@@ -2,6 +2,35 @@
 library(ggplot2)
 library(caret)
 
+## ------------------------------------------------------------------------
+training = read.csv("./pml-training.csv",na.strings=c("NA",""))
+testing = read.csv("./pml-testing.csv",na.strings=c("NA",""))
+
+## ------------------------------------------------------------------------
+qplot(x=X,y=classe,data=training);
+training = subset(training, select = -c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,new_window))
+testing = subset(testing, select = -c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,new_window))
+
+## ------------------------------------------------------------------------
+n = nrow(testing)
+ratio = 0.5
+keep = colSums(is.na(training))<ratio*n
+training = training[, keep]
+testing  = testing [, keep]
+
+## ------------------------------------------------------------------------
+k = 5
+set.seed(42)
+
+InTrain<-createDataPartition(y=training$classe,p=0.01,list=FALSE)
+df<-training[InTrain,]
+
+model = train(classe ~ ., method = 'rf', data = df, trControl=trainControl(method="cv",number=k), allowParallel=TRUE,prox=TRUE)
+print(model$finalModel)
+
+## ------------------------------------------------------------------------
+predictions = predict(model, newdata = testing)
+
 ## ----code=readLines(knitr::purl('./predicting_activities.Rmd', documentation = 1)), eval = FALSE----
 ## NA
 
